08c6b1ff088caad215788c55811daf41952f06ca
gpavlik99 <61884971+gpavlik99@users.noreply.github.com>
2026-01-20 16:51:41 -0500

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { supabase } from "@/integrations/supabase/client";
+import { supabase } from "@/integrations/client";
 import { useToast } from "@/hooks/use-toast";
 import { ChurchIcon, Loader2 } from "lucide-react";
 import { z } from "zod";
